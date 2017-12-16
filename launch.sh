bcd09de2f5b53f7e865667308cbb0047e711ee0f
Max Milton <max@wearegenki.com>
2017-12-16 15:36:50 +1100

    New launch script; better security, fix shm size, allow passing custom cmd, rename seccomp conf

--- launch.sh
+++ launch.sh
@@ -5,30 +5,28 @@ set -eo errtrace
 trap 'xhost -local:$USER' EXIT
 
 # directory of this script
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 
-# random
 # allow X11 forwarding permission
 xhost +local:"$USER"
 
-# TODO: Replacement for --net host
-# TODO: Prevent dbus errors and crash
-
-# minimal; no persistence or sound
-docker run -it --rm \
+# minimal chromium; no persistence
+docker run \
+  --rm \
   --name chromium \
-  --net host \
+  --network host \
   --cpuset-cpus 0 \
   --memory 512mb \
   --read-only \
   --volume /tmp/.X11-unix:/tmp/.X11-unix:z \
   --env DISPLAY=unix"$DISPLAY" \
   --device /dev/snd \
-  --volume /dev/shm:/dev/shm \
-  --tmpfs /home/chromium:rw,noexec,nodev,nosuid,uid=6006,gid=6006,mode=0700,size=256m \
+  --volume /dev/shm:/dev/shm:z \
+  --tmpfs /run:rw,nosuid,nodev \
+  --tmpfs /tmp:rw,nosuid,nodev \
+  --tmpfs /data:rw,noexec,nosuid,nodev,uid=6006,gid=6006,mode=0700 \
+  --tmpfs /home/chromium:rw,nosuid,nodev,uid=6006,gid=6006,mode=0700,size=4m \
   --cap-drop=all \
   --security-opt no-new-privileges \
-  --security-opt seccomp="$DIR"/chrome-seccomp.json \
-  local/chromium
-  # FIXME: Results in chromium not able to write to /home/chromium
-  # --read-only \
+  --security-opt seccomp="$DIR"/seccomp.json \
+  local/chromium $@
