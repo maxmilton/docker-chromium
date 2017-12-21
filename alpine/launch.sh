2f5d1106fe74968a1e835d35daccbd3f233397e1
Max Milton <max@wearegenki.com>
2017-12-22 10:30:09 +1100

    Add launch script for alpine image

---
+++ alpine/launch.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# revoke X11 forwarding permission on exit
+set -eo errtrace
+trap 'xhost -local:$USER' EXIT
+
+# directory of this script
+DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+
+# allow X11 forwarding permission
+xhost +local:"$USER"
+
+# minimal chromium; no persistence
+docker run \
+  --rm \
+  --name chromium-alpine \
+  --network host \
+  --cpuset-cpus 0 \
+  --memory 512m \
+  --read-only \
+  --volume /tmp/.X11-unix:/tmp/.X11-unix:z \
+  --env DISPLAY=unix"$DISPLAY" \
+  --group-add "$(getent group audio | cut -d: -f3)" \
+  --device /dev/snd \
+  --volume /dev/shm:/dev/shm:z \
+  --tmpfs /run:rw,nosuid,nodev \
+  --tmpfs /tmp:rw,nosuid,nodev \
+  --tmpfs /data:rw,noexec,nosuid,nodev,uid=6006,gid=6006,mode=0700 \
+  --tmpfs /home/chromium:rw,nosuid,nodev,uid=6006,gid=6006,mode=0700,size=4m \
+  --cap-drop=all \
+  --security-opt no-new-privileges \
+  --security-opt seccomp="$DIR"/../seccomp.json \
+  local/chromium-alpine $@
